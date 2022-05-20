3255228512cfa991ff88f462f673906cf7cb554b
megafinz <megafinz@gmail.com>
2022-05-20 17:53:48 +0300

    Reorganize code a little

---
+++ HttpApi/handlers.ts
@@ -0,0 +1,66 @@
+import { VatRequest } from '../models';
+import * as vies from '../lib/vies';
+import * as db from '../lib/db';
+
+const { MAX_PENDING_VAT_NUMBERS_PER_USER } = process.env;
+const maxPendingVatNumbersPerUser = parseInt(MAX_PENDING_VAT_NUMBERS_PER_USER);
+
+export type Result = { success: boolean; message: string };
+
+export async function check(vatRequest: VatRequest): Promise<Result> {
+    try {
+        await vies.init();
+        const result = await vies.checkVatNumber(vatRequest);
+
+        if (result.valid) {
+            return { success: true, message: `VAT number '${vatRequest.countryCode}${vatRequest.vatNumber}' is valid.` };
+        } else {
+            await db.init();
+
+            const existingVatRequest = await db.findVatRequest(vatRequest);
+
+            if (!existingVatRequest) {
+                const currentPendingVatNumbers = await db.countVatRequests(vatRequest.telegramChatId);
+
+                if (currentPendingVatNumbers < maxPendingVatNumbersPerUser) {
+                    await db.addVatRequest(vatRequest);
+                } else {
+                    return { success: false, message: `Sorry, you reached the limit of maximum VAT numbers you can monitor (${maxPendingVatNumbersPerUser}).` };
+                }
+            }
+
+            return { success: true, message: `VAT number '${vatRequest.countryCode}${vatRequest.vatNumber}' is not registered in VIES yet. You will be notified when it becomes valid.` };
+        }
+    } catch (error) {
+        // TODO: recognize more unrecoverable errors
+        if (error.message?.includes("INVALID_INPUT")) {
+            return { success: false, message: `There was a problem validating your VAT number '${vatRequest.countryCode}${vatRequest.vatNumber}'. Make sure it is in the correct format. This is the error message that we got from VIES:\n\n${error}.` }
+        } else {
+            await db.addVatRequest(vatRequest);
+            return { success: false, message: `There was a problem validating your VAT number '${vatRequest.countryCode}${vatRequest.vatNumber}'. We'll keep monitoring it for a while. This is the error message that we got from VIES:\n\n${error}.` };
+        }
+    }
+}
+
+export async function uncheck(vatRequest: VatRequest): Promise<Result> {
+    await db.init();
+    await db.removeVatRequest(vatRequest);
+    return { success: true, message: `VAT number '${vatRequest.countryCode}${vatRequest.vatNumber}' is no longer being monitored.` };
+}
+
+export async function list(telegramChatId: string): Promise<Result> {
+    await db.init();
+    const result = await db.getAllVatRequests(telegramChatId);
+
+    if (result.length > 0) {
+        return { success: true, message: `You monitor the following VAT numbers:\n\n${result.map(vr => `'${vr.countryCode}${vr.vatNumber}'`).join(', ')}.` };
+    } else {
+        return { success: true, message: "You are not monitoring any VAT numbers." };
+    }
+}
+
+export async function uncheckAll(telegramChatId: string): Promise<Result> {
+    await db.init();
+    await db.removeAllVatRequests(telegramChatId);
+    return { success: true, message: "You no longer monitor any VAT numbers." };
+}
--- HttpApi/index.ts
+++ HttpApi/index.ts
@@ -1,13 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { VatRequest } from "../models";
-import * as db from "../lib/db";
-import * as vies from "../lib/vies";
-
-const { MAX_PENDING_VAT_NUMBERS_PER_USER } = process.env;
-const maxPendingVatNumbersPerUser = parseInt(MAX_PENDING_VAT_NUMBERS_PER_USER);
+import * as handlers from './handlers';
 
 type Action = "check" | "uncheck" | "uncheckAll" | "list";
-type Result = { success: boolean; message: string };
 
 const allowedActions: Action[] = [
     "check",
@@ -20,7 +14,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const telegramChatId = req.query.telegramChatId || req.body?.telegramChatId;
     const action = <Action>req.params.action;
 
-    let result = null;
+    let result: handlers.Result = null;
 
     if (!telegramChatId) {
         result = { success: false, message: "Missing Telegram Chat ID" };
@@ -49,15 +43,17 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                     vatNumber: vatNumber
                 };
 
-                result = action === 'check' ? await check(vatRequest) : await uncheck(vatRequest);
+                result = action === 'check'
+                    ? await handlers.check(vatRequest)
+                    : await handlers.uncheck(vatRequest);
                 break;
 
             case "uncheckAll":
-                result = await uncheckAll(telegramChatId);
+                result = await handlers.uncheckAll(telegramChatId);
                 break;
 
             case "list":
-                result = await list(telegramChatId);
+                result = await handlers.list(telegramChatId);
                 break;
         }
 
@@ -69,63 +65,5 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         };
     }
 };
-
-async function check(vatRequest: VatRequest): Promise<Result> {
-    try {
-        await vies.init();
-        const result = await vies.checkVatNumber(vatRequest);
-
-        if (result.valid) {
-            return { success: true, message: `VAT number '${vatRequest.countryCode}${vatRequest.vatNumber}' is valid.` };
-        } else {
-            await db.init();
-
-            const existingVatRequest = await db.findVatRequest(vatRequest);
-
-            if (!existingVatRequest) {
-                const currentPendingVatNumbers = await db.countVatRequests(vatRequest.telegramChatId);
-
-                if (currentPendingVatNumbers < maxPendingVatNumbersPerUser) {
-                    await db.addVatRequest(vatRequest);
-                } else {
-                    return { success: false, message: `Sorry, you reached the limit of maximum VAT numbers you can monitor (${maxPendingVatNumbersPerUser}).` };
-                }
-            }
-
-            return { success: true, message: `VAT number '${vatRequest.countryCode}${vatRequest.vatNumber}' is not registered in VIES yet. You will be notified when it becomes valid.` };
-        }
-    } catch (error) {
-        // TODO: recognize more unrecoverable errors
-        if (error.message?.includes("INVALID_INPUT")) {
-            return { success: false, message: `There was a problem validating your VAT number '${vatRequest.countryCode}${vatRequest.vatNumber}'. Make sure it is in the correct format. This is the error message that we got from VIES:\n\n${error}.` }
-        } else {
-            await db.addVatRequest(vatRequest);
-            return { success: false, message: `There was a problem validating your VAT number '${vatRequest.countryCode}${vatRequest.vatNumber}'. We'll keep monitoring it for a while. This is the error message that we got from VIES:\n\n${error}.` };
-        }
-    }
-}
-
-async function uncheck(vatRequest: VatRequest): Promise<Result> {
-    await db.init();
-    await db.removeVatRequest(vatRequest);
-    return { success: true, message: `VAT number '${vatRequest.countryCode}${vatRequest.vatNumber}' is no longer being monitored.` };
-}
-
-async function list(telegramChatId: string): Promise<Result> {
-    await db.init();
-    const result = await db.getAllVatRequests(telegramChatId);
-
-    if (result.length > 0) {
-        return { success: true, message: `You monitor the following VAT numbers:\n\n${result.map(vr => `'${vr.countryCode}${vr.vatNumber}'`).join(', ')}.` };
-    } else {
-        return { success: true, message: "You are not monitoring any VAT numbers." };
-    }
-}
-
-async function uncheckAll(telegramChatId: string): Promise<Result> {
-    await db.init();
-    await db.removeAllVatRequests(telegramChatId);
-    return { success: true, message: "You no longer monitor any VAT numbers." };
-}
 
 export default httpTrigger;
